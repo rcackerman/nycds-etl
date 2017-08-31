b3e8369bea28e4ed3e0e6616dbb904467a3f6508
Rebecca Ackerman <rcackerman@gmail.com>
2017-08-31 17:29:07 -0400

    Adding in multiple tables loop

--- load_csvs.py
+++ load_csvs.py
@@ -1,7 +1,16 @@
 import csv
 import psycopg2
 
-FILENAME = '.\dispo2_cleaned.csv'
+FILENAMES = {'addresses_cleaned.csv': 'addresses',
+             'custodystatus_cleaned.csv': 'custody_statuses',
+             'dispo2_cleaned.csv': 'dispositions',
+             'events_cleaned.csv': 'events',
+             'memomain_cleaned.csv': 'main_memos',
+             'names_cleaned.csv': 'names',
+             'results_cleaned.csv': 'results',
+             'sentences_cleaned.csv': 'sentences',
+             'loi_cleaned.csv': 'loi',
+             'cases_cleaned.csv': 'cases'}
 
 SQL_STATEMENT = """
     COPY %s FROM STDIN WITH
@@ -22,5 +31,7 @@ def copy_csv(connection, table_name, csv_file):
 
 conn = psycopg2.connect("dbname=pdcms user=postgres port=5433")
 
-with open(FILENAME, 'rb') as f:
-    copy_csv(conn, 'dispositions', f)
+for k, v in FILENAMES.iteritems():
+    print v
+    with open(k, 'rb') as f:
+        copy_csv(conn, v, f)
