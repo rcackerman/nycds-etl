dd675d47f5b2c805557e436f821701d45f3cc2f6
Rebecca Ackerman <rcackerman@gmail.com>
2017-08-31 15:07:54 -0400

    Separating rows with more nuance
    Fixes an issue where if an \r\n was followed by a quote mark and
    numbers, it would split to a new row of the CSV, regardless of whether
    that was actually a real row.

--- clean_csvs.py
+++ clean_csvs.py
@@ -1,7 +1,16 @@
 import re, csv
 
-INFILENAME = 'dispo2'
-OUTFILENAME = INFILENAME + '_cleaned'
+def split_lines(file_object):
+    """Split lines based on the number of columns in the header line.
+    """
+    file_text = file_object.read()
+    n = len(file_text.splitlines()[0].split('\t')) - 1
+
+    # Multiplying by the number of fields - 1 so we don't have to deal with
+    # re.split() dealing weirdly with capturing groups.
+    # This is hack-y code. Don't do this at home.
+    line = re.compile('\r\n(?="' + '[^\t\n]*?\t'* n + '[^\t\n]*)')
+    return re.split(line, file_text)
 
 def escape_quotes(row):
     """Escapes any rows with misplaced quotes
@@ -19,9 +28,16 @@ def strip_surrounding_quotes(row):
     row = [re.sub(r'^"|"$', '', field) for field in row]
     return row
 
+
+INFILENAME = 'results'
+OUTFILENAME = INFILENAME + '_cleaned'
+
 # To split correctly, look ahead for a quote mark followed by a number.
 with open('{}.csv'.format(INFILENAME), 'rb') as f:
-    CSV_ROWS = re.split(r'\r\n(?="[0-9A])', f.read())
+    CSV_ROWS = split_lines(f)
+
+# A blank last line will leave an extra \r\n in the final row, so delete it.
+CSV_ROWS[-1] = re.sub(r'""\r\n', '""', CSV_ROWS[-1])
 
 CSV_ROWS = [escape_quotes(row) for row in CSV_ROWS]
 CSV_ROWS = [strip_surrounding_quotes(row) for row in CSV_ROWS]
