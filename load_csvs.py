1b36bbe64ca47cabd6d8f0141e33ea3ad39b6e26
Rebecca Ackerman <rcackerman@gmail.com>
2017-09-06 14:17:20 -0400

    Adding code to delete old records and check success

--- load_csvs.py
+++ load_csvs.py
@@ -31,10 +31,33 @@ def copy_csv(connection, table_name, csv_file):
     connection.commit()
     cur.close()
 
-
 conn = psycopg2.connect("dbname=pdcms user=postgres port=5433")
 
 for k, v in FILENAMES.items():
     print(v)
-    with open(k, 'rb') as f:
-        copy_csv(conn, v, f)
+    del_string = "DELETE FROM {};".format(v)
+    select_string = "SELECT COUNT(*) FROM {};".format(v)
+
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(del_string)
+
+    # Check that delete worked
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(select_string)
+            print(cursor.fetchall())
+
+    with conn:
+        with conn.cursor() as cursor:
+            with open(k, 'rb') as f:
+                cursor.copy_expert(sql=SQL_STATEMENT % v, file=f)
+
+    # Check that copy worked
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(select_string)
+            print(cursor.fetchall())
+
+conn.close()
+    # copy_csv(conn, v, f)
