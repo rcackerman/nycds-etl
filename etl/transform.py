fd59776c7b8a1392c96e7a062aafda4893e46f9a
Rebecca Ackerman <rcackerman@gmail.com>
2018-03-29 11:18:40 -0400

    Starting on date fixing

--- etl/transform.py
+++ etl/transform.py
@@ -3,4 +3,12 @@
 import pandas
 import numpy
 
-def 
+def fix_dates(self):
+"""Some fixes for dates:
+    * delete if the month and day is the same as another, correct year
+    *   collapse the purposes if necessary
+    * a few dates with 2008 became 1864 for some reason
+    * if the year is just missing first 2 digits, add those
+    * if all events except the bad one happen in a single year, use that year
+
+
