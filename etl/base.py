9cffea42c8dbfb95e7c2534436094bd20a272ee0
Rebecca Ackerman <rcackerman@gmail.com>
2018-04-11 14:51:45 -0400

    Drying up base model

--- etl/base.py
+++ etl/base.py
@@ -8,17 +8,15 @@ CFG = yaml.load(open('config.yaml', 'r'))
 
 class ETLDataTable():
 
-    def __init__(self, table_name):
+    def __init__(self, table_name, file_name, columns_list=None):
         # Name of the database table
-        self.table_name = CFG[table_name]['table']
+        self.table_name = table_name
         # Name of the CSV file
-        self.file_name = CFG[table_name]['file']
-        # Whether file has note fields
-        self.notes = CFG[table_name]['notes']
+        self.file_name = file_name
 
         #Data Types to feed into read_csv
         try:
-            self.columns = CFG[table_name]['columns']
+            self.columns = columns_list
         except:
             pass
 
@@ -38,13 +36,12 @@ class ETLDataTable():
         """
         self.frame = self.frame.replace(r'^\s+$', numpy.NaN, regex=True)
 
-    def coerce_to_date(self):
+    def coerce_to_date(self, date_columns=None):
         """Coerce the columns listed in the config file into Pandas datetime
         so they can be transferred to PostgreSQL in that format. Drop any
         dates that can't be coerced.
         """
-        dcols = CFG[self.table_name]['date_columns']
-        self.frame[dcols] = self.frame[dcols].transform(
+        self.frame[date_columns] = self.frame[date_columns].transform(
                 lambda x: pandas.to_datetime(x,
                                              format='%Y%m%d',
                                              errors='coerce'))
@@ -52,4 +49,7 @@ class ETLDataTable():
     def save_to_table(self, conn, **kwargs): 
         """Save frame to a PostgreSQL, using Pandas built-in to_sql() method.
         """
-        self.frame.to_sql(name=self.table_name, con=conn, **kwargs)
+        self.frame.to_sql(name=self.table_name,
+                          con=conn,
+                          if_exists = 'replace',
+                          **kwargs)
--- etl/etl.py
+++ etl/etl.py
@@ -1,19 +1,32 @@
 """Extract data from CSVs into Pandas dataframes
 """
 
-import pandas
 from os import path
+import pandas
+import numpy
+import yaml
+from sqlalchemy import create_engine
 from base import *
+
+CFG = yaml.load(open('config.yaml', 'r'))
 
 ETL_ROOT_DIR = path.abspath(path.pardir)
 ENGINE = create_engine('postgresql://postgres@localhost:5433/pdcms')
 
 for key in CFG.keys():
-    table = DataTable(key).load_df(ETL_ROOT_DIR)
-    print(dir(table))
+    # Extract
+    table = ETLDataTable(CFG[key]['table'], CFG[key]['file'])
+    table.load_df()
     # table = table.strip_strings()
-    print(table.head())
+    print(table.frame.head())
 
-conn = engine.connect()
+    # Transform
+    table.strip_strings()
+    table.coerce_to_date(CFG[key]['date_columns'])
+
+    # Load
+    conn = ENGINE.connect()
+    table.save_to_table(conn)
+    conn.close()
 
 
